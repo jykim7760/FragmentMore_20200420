01f56e354a6aa947b3d6901bcfe7e01ebd1c060c
jykim7760 <jykim7760@naver.com>
2020-04-20 21:01:29 +0900

    뷰페이져어댑터클레스 변경  - 3페이지 처리   - return when 문법 활용

--- app/src/main/java/kr/tjeit/fragmentmore_20200420/boardFragment.kt
+++ app/src/main/java/kr/tjeit/fragmentmore_20200420/boardFragment.kt
@@ -1,11 +1,54 @@
 package kr.tjeit.fragmentmore_20200420
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_board.*
+import kr.tjeit.fragmentmore_20200420.adapters.PostAdapter
+import kr.tjeit.fragmentmore_20200420.datas.Post
+import kr.tjeit.fragmentmore_20200420.fragments.BaseFragment
 
-class boardFragment : Fragment(){
+class boardFragment : BaseFragment(){
 
+    var mPostAdapter : PostAdapter? = null
+    var mPostList : ArrayList<Post>()
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_game_list, container, false)
 
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
+
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+        mPostList.add(Post(companyName = "삼성전자",  Amount = 10, UserName = "김지영"))
+        mPostList.add(Post(companyName = "삼성즌자",  Amount = 10, UserName = "김지영"))
+        mPostList.add(Post(companyName = "삼성잔자",  Amount = 10, UserName = "김지영"))
+        mPostList.add(Post(companyName = "삼성친자",  Amount = 10, UserName = "김지영"))
+        mPostList.add(Post(companyName = "삼성주자",  Amount = 10, UserName = "김지영"))
+        mPostList.add(Post(companyName = "삼성차자",  Amount = 10, UserName = "김지영"))
+        mPostList.add(Post(companyName = "삼성빵자",  Amount = 10, UserName = "김지영"))
+        mPostList.add(Post(companyName = "삼성즐자",  Amount = 10, UserName = "김지영"))
+
+        mPostAdapter = PostAdapter(mContext!!, R.layout.post_list_item, mPostList)
+        boardListView.adapter = mPostAdapter
+    }
 
 
 }
--- app/src/main/java/kr/tjeit/fragmentmore_20200420/datas/Post.kt
+++ app/src/main/java/kr/tjeit/fragmentmore_20200420/datas/Post.kt
@@ -3,13 +3,8 @@ package kr.tjeit.fragmentmore_20200420.datas
 import android.util.Log
 
 class Post (var companyName : String,
-            var Amount : Int, var UserName : String){
-
-
-
-
-    }
-
+            var Amount : Int, var UserName : String)
+{
 
 
 }
