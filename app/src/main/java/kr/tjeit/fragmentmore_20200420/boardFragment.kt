e5f955632cc71d606bc65c4534cd5f057726f31b
jykim7760 <jykim7760@naver.com>
2020-04-20 20:42:43 +0900

    거래게시판 리스트뷰 기초작업중  - 데이터 클래스, XML

---
+++ app/src/main/java/kr/tjeit/fragmentmore_20200420/boardFragment.kt
@@ -0,0 +1,11 @@
+package kr.tjeit.fragmentmore_20200420
+
+import androidx.fragment.app.Fragment
+
+class boardFragment : Fragment(){
+
+
+
+
+
+}
---
+++ app/src/main/java/kr/tjeit/fragmentmore_20200420/datas/Post.kt
@@ -0,0 +1,15 @@
+package kr.tjeit.fragmentmore_20200420.datas
+
+import android.util.Log
+
+class Post (var companyName : String,
+            var Amount : Int, var UserName : String){
+
+
+
+
+    }
+
+
+
+}
